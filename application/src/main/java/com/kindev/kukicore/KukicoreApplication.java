7848c025a663bc530d9fed53ad35b49cd04f4e39
princebak <prince.ilunga@businesslighthouse.tech>
2021-10-14 10:14:28 +0100

    cross origins enabled and mvn version changed

--- application/src/main/java/com/kindev/kukicore/KukicoreApplication.java
+++ application/src/main/java/com/kindev/kukicore/KukicoreApplication.java
@@ -3,6 +3,9 @@ package com.kindev.kukicore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,5 +16,15 @@ public class KukicoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(KukicoreApplication.class, args);
         log.info("CustomerApplication Started........");
+    }
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
